98f1ec96d73a336c729f250886b1797e65218c60
DHARANI MAHATA <126614898+Dharani321@users.noreply.github.com>
2024-08-28 01:47:26 +0530

    Add files via upload

---
+++ ca.js
@@ -0,0 +1,38 @@
+//var result=document.querySelector("input");\
+// document.querySelector("button").addEventListener(("click"),function(){
+// console.log(document.querySelector("button").innerHTML);
+// })
+// addEventListener("click",function()
+// {
+//    var c =button.value;
+//    result.value=c;
+//    console.log(result.value);
+
+// })
+
+var result=document.querySelector("input");
+function fun(num)
+{
+    result.value+=num;
+}
+function fun1()
+{
+    result.value="";
+}
+function del()
+{
+    result.value=result.value.slice(0,-1);
+}
+
+function calculate()
+{  
+    var a=result.value;
+    var b=eval(a);
+    result.value=b;
+}
+// document.addEventListener("keypress",function(){
+//     //alert("keypress");
+//     var a=result.value;
+//     var b=eval(a);
+//     result.value=b;
+// })
